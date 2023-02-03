bc762ebdad7f64191bc3d19c1470b77e45edb984
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-02-03 18:12:35 -0300

    Criando componente de Cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,14 +4,28 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default _ =>
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min="0" max="100"/>
-        <Fragmento></Fragmento>
-        <ComParametro titulo="Situação Aluno" aluno="Pedro" nota={6} />
-        <ComParametro titulo="Situação Aluno" aluno="Maria" nota={9.7} />
-        <Primeiro />
+
+        <Card titulo="#4 - Desafio Aleátorio">
+            <Aleatorio min="0" max="100" />
+        </Card>
+
+        <Card titulo="#3 - Fragmento React">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#2 - Situação do Aluno">
+            <ComParametro titulo="Situação Aluno" aluno="Pedro" nota={6} />
+            <ComParametro titulo="Situação Aluno" aluno="Maria" nota={9.7} />
+        </Card>
+
+        <Card titulo="#1 - Primeiro Componente">
+            <Primeiro />
+        </Card>
+
     </div>
 
