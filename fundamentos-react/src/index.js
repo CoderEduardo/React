406f417f9f006c6a56cdf4a25cfdbd20c95bab59
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-02-03 15:47:54 -0300

    Criando componente com parametros

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
 ReactDOM.render(
    <div>
      <Primeiro/>
+     <ComParametro titulo="Situação Aluno" aluno="Pedro" nota={6}/>
+     <ComParametro titulo="Situação Aluno" aluno="Maria" nota={9.7}/>
    </div> ,
     document.getElementById("root")
 )
