efacb449106fa21c74cd3e9960607987192bfe7c
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-22 10:49:31 -0300

    Criando o meu primeiro componente

---
+++ learn/src/componentes/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css'
+
+function Header(){
+    return(
+        <header className="header">
+        <h1>Luis Eduardo</h1>
+        <ul className="lista">
+            <li>Sobre</li>
+            <li>Casa</li>
+            <li>Contato</li>
+        </ul>
+    </header>
+    )
+}
+
+export default Header
