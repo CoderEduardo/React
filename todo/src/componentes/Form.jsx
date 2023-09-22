f37e0c272f1e4fc570dcac413192adf92f36d696
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-22 15:22:38 -0300

    Adicionando tarefas ao todo

--- todo/src/componentes/Form.jsx
+++ todo/src/componentes/Form.jsx
@@ -4,19 +4,30 @@ import { useState } from "react";
 function Form() {
 
     const [texto, setTexto] = useState("")
+    const [itens, setItens] = useState([])
 
-    function escrever(evento){
-        let conteudo = evento.target.value 
+    function escrever(evento) {
+        let conteudo = evento.target.value
         setTexto(conteudo)
-    }   
+    }
+
+    function adicionarItem(evento) {
+        evento.preventDefault()
+        if (texto) {
+            setItens([...itens, texto])
+            setTexto("")
+        }
+    }
 
     return (
         <>
             <form>
-                <input type="text" onChange={escrever} />
+                <input type="text" onChange={escrever} value={texto} />
+                <button onClick={adicionarItem}>Add</button>
             </form>
+
             <ul>
-                <li>{texto}</li>
+                {itens.map(iten => <li>{iten}</li>)}
             </ul>
         </>
 
