e3d3729c475c5d890b0a0aaf1a99bcd9526ea083
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-25 11:36:21 -0300

    Usando mais de um reducer

--- Redux/index.js
+++ Redux/index.js
@@ -3,13 +3,15 @@ um middleware, agindo no meio das actions e do reducer, temos o reducer que mani
 
 const redux = require("redux")
 const createStore = redux.createStore
+//Método para usar mais de um reducer
+const combineReducer = redux.combineReducers
 
 //Criando um objeto para atribuir uma ação
 const incrementAction = (value = 1) => {
-    return {type:"INCREMENT",payload:value}
+    return { type: "INCREMENT", payload: value }
 }
-const decrementAction = (value = 1) =>{
-    return {type:"DECREMENT", payload:value}
+const decrementAction = (value = 1) => {
+    return { type: "DECREMENT", payload: value }
 }
 
 //criando o reducer
@@ -27,13 +29,36 @@ function counterReducer(state = 0, action) {
 
 }
 
+//--------------------------------
+
+//Action que adciona um novo item
+const addItemAction = (item)=>{
+    return {type:"ADD_ITEM",payload:item}
+}
+
+//Reducer que recebe a action que altera o estado
+const listReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_ITEM':
+            return [...state, action.payload]
+        default:
+            return state
+    }
+}
+
+const allReducers = combineReducer({
+    counter:counterReducer,
+    list:listReducer
+})
+
 //criando a nossa store com o proprio método
-const store = createStore(counterReducer)
+const store = createStore(allReducers)
 
 //atualizando sempre que o alguna coisa é atualizada
 store.subscribe(() => {
     console.log(store.getState())
 })
 
+store.dispatch(addItemAction("Um novo item"))
 store.dispatch(decrementAction(10))
 
